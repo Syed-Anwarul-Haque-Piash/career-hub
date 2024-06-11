21de76079df6f8fee3ddf33bcc8467e8c03caa19
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2024-06-11 15:16:02 +0600

    see all data

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Job from '../Job/Job';
 
 const FeaturedJobs = () => {
-    const [jobs,setJobs]=useState([])
-    useEffect(()=>{
+    const [jobs, setJobs] = useState([])
+    useEffect(() => {
         fetch('jobs.json')
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            setJobs(data)
-        })
-    },[])
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setJobs(data)
+            })
+    }, [])
+    const [data, setData] = useState(4)
     return (
         <div>
             <h1 className='text-center font-bold text-4xl'>Featured Jobs</h1>
             <p className='text-gray-400 text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             <div className='md:grid grid-cols-2 gap-4 p-4 ml-6'>
-                {jobs.map(job=><Job job={job}></Job>)}
+                {jobs.slice(0, data).map(job => <Job job={job}></Job>)}
+            </div>
+            <div className={data===jobs.length?'hidden':''}>
+                <button onClick={() => setData(jobs.length)} className='btn btn-primary  p-4 mx-auto text-center'>See All Jobs</button>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-const Job = ({job}) => {
-    const {id,logo,job_title,company_name,remote_or_onsite,location,job_type,salary,job_description,job_responsibility}=job;
+const Job = ({ job }) => {
+    const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary, job_description, job_responsibility } = job;
     return (
-        <div >
-            <img className='h-9 w-56' src={logo} alt="" />
-            <h1 className='text-2xl font-bold'>{job_title}</h1>
-            <button className='bg-violet-300 text-white font-bold p-3 rounded mt-4 mr-4'>{remote_or_onsite}</button>
-            <button className='bg-violet-300 text-white font-bold p-3 rounded mt-4'>{job_type}</button>
-            <div className='md:flex'>
-                <h1 className='text-gray-400'>{location}</h1>
-                <h1 className='text-gray-400 ml-4'>{salary}</h1>
+        <div className='mt-8'>
+            <div>
+                <img className='h-9 w-56' src={logo} alt="" />
+                <h1 className='text-2xl font-bold'>{job_title}</h1>
+                <button className='bg-violet-300 text-white font-bold p-2 rounded mt-4 mr-4'>{remote_or_onsite}</button>
+                <button className='bg-violet-300 text-white font-bold p-2 rounded mt-4'>{job_type}</button>
+                <div className='md:flex'>
+                    <h1 className='text-gray-400'>{location}</h1>
+                    <h1 className='text-gray-400 ml-4'>{salary}</h1>
+                </div>
+                <button className='bg-violet-300 text-white font-bold p-2 rounded mt-4'>View Details</button>
             </div>
-            <button className='bg-violet-300 text-white font-bold p-3 rounded mt-4'>View Details</button>
             
+
+
         </div>
     );
 };
