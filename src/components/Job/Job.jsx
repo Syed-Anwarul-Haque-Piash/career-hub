a5db63259d437edbba0ee01c2acd001b8af5eccd
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2024-06-11 11:40:03 +0600

    display all job

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -6,8 +6,13 @@ const Job = ({job}) => {
         <div >
             <img className='h-9 w-56' src={logo} alt="" />
             <h1 className='text-2xl font-bold'>{job_title}</h1>
-            <button className='border-violet-400 rounded color-violet-400 mr-4'>{remote_or_onsite}</button>
-            <button className='border-violet-400'> {job_type}</button>
+            <button className='bg-violet-300 text-white font-bold p-3 rounded mt-4 mr-4'>{remote_or_onsite}</button>
+            <button className='bg-violet-300 text-white font-bold p-3 rounded mt-4'>{job_type}</button>
+            <div className='md:flex'>
+                <h1 className='text-gray-400'>{location}</h1>
+                <h1 className='text-gray-400 ml-4'>{salary}</h1>
+            </div>
+            <button className='bg-violet-300 text-white font-bold p-3 rounded mt-4'>View Details</button>
             
         </div>
     );
